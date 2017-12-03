70db69135540f716bcdebb34ab11726cf83d6e6e
Desarrollo BioBioChile <emunoz.ccp@gmail.com>
2017-12-03 01:51:50 -0300

    Initial Commit

---
+++ glTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  glTest
+//
+//  Created by Esteban Muñoz Lagos on 03-12-17.
+//  Copyright © 2017 Esteban Muñoz Lagos. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
