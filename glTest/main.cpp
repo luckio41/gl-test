aba2e20bf889ec2aea547a9882ce51c279171159
Desarrollo BioBioChile <emunoz.ccp@gmail.com>
2017-12-03 04:02:09 -0300

    First Commit!

--- glTest/main.cpp
+++ glTest/main.cpp
@@ -7,9 +7,53 @@
 //
 
 #include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+void render(void) {
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    glRotatef(45.0, 0.0, 1.0, 0.0);
+    
+    glBegin(GL_TRIANGLES); {
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glVertex2f(0.0f, 0.5f);
+        glColor3f(0.0f, 1.0f, 0.0f);
+        glVertex2f(-0.5f, -0.5f);
+        glColor3f(0.0f, 0.0f, 1.0f);
+        glVertex2f(0.5f, -0.5f);
+    }
+    glEnd();
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    GLFWwindow *win;
+    
+    if(!glfwInit()) {
+        return -1;
+    }
+    
+    win = glfwCreateWindow(640, 480, "OpenGl Test", NULL, NULL);
+    if(!win) {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+    
+    if(!glewInit()) {
+        return -1;
+    }
+    
+    glfwMakeContextCurrent(win);
+    
+    while (!glfwWindowShouldClose(win)) {
+        render();
+        glfwSwapBuffers(win);
+        glfwPollEvents();
+    }
+    
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+    
     return 0;
 }
